84ff9d8419fb8b8935c20d75c9bffee3fc20c5c5
Jess Telford <jess+github@jes.st>
2016-11-28 22:59:44 +1100

    Fleshing out catching section

--- src/slides/catching.js
+++ src/slides/catching.js
@@ -13,7 +13,25 @@ const SlideNotes = (props) => (
 
 export default (props) => (
   <ContentSlide>
-    Catching
+    <h1>Catching</h1>
+
+    * "Let's start by talking about 3D"
+    * "Done in the browser via WebGL (is hard)"
+    * Show demo of static scene with ball + monster + background (no tilt
+      controls)
+    * "Simple scene should have simple code: It does, thanks to aframe!"
+    * <a-scene>...</a-scene>
+    * "Aframe also does tilt controls"
+    * Demo of same scene with tilt controls enabled
+    * <a-scene><a-camera whatever-look-controls /></a-scene>
+    * "Aframe even comes with free VR support!"
+    * Click the VR mode, pop phone into headset and look around for 10 seconds
+    * "But looking at the monster is boring, we want to catch it!"
+    * Click + drag the ball, then toss it at the monster
+    * <a-scene physics><a-sphere click-drag /></a-scene>
+    * "Cannon.js for physics"
+    * "This is the power of aframe!"
+
     {props.showNotes ? <SlideNotes {...props} /> : ''}
   </ContentSlide>
 );
