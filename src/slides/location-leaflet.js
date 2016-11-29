3e4d347ba13df6394511c87c4484873dea7bd181
Jess Telford <jess+github@jes.st>
2016-11-29 23:19:19 +1100

    Begging of slides for Location Aware section

---
+++ src/slides/location-leaflet.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Code, Step } from 'react-presents'
+import Notes from './templates/notes';
+import Slide from './templates/slide';
+
+const StyledSlide = styled(Slide)`
+  text-align: center;
+  background-repeat: no-repeat;
+  background-position: right 50% top 30%;
+  background-size: auto 40vh;
+  background-image: url('./img/leaflet.png');
+`;
+
+const LinkContainer = styled.div`
+  position: relative;
+  top: 25vh;
+  font-size: 6rem;
+`;
+
+const SlideNotes = (props) => (
+  <Notes {...props}>
+    <ul>
+      <li>GMaps alternative</li>
+      <li>Recently 1.0 after years</li>
+      <li>&raquo; Simple scene == simple code</li>
+    </ul>
+  </Notes>
+)
+
+export default (props) => (
+  <StyledSlide>
+    <LinkContainer>
+      <a href="http://leafletjs.com">leafletjs.com</a>
+    </LinkContainer>
+    {props.showNotes ? <SlideNotes {...props} /> : ''}
+  </StyledSlide>
+);
--- src/slides/location-title.js
+++ src/slides/location-title.js
@@ -1,25 +1,15 @@
 import React from 'react'
 import styled from 'styled-components';
-import { ContentSlide, Step } from 'react-presents'
+import { Step } from 'react-presents'
+import Subtitle from './templates/subtitle';
 import Notes from './templates/notes';
 
 const SlideNotes = (props) => (
   <Notes {...props}>
     <ul>
-      <li>...</li>
-    </ul>
-  </Notes>
-)
+      <li>Maps apps like Google / Apple are obv.</li>
+      <li>OpenStreetMap too! FOSS <i>data only</i>.</li>
 
-export default (props) => (
-  <ContentSlide>
-    <h1>Location Aware</h1>
-
-    <ul>
-      <li>"GIS (Geographic Information Systems) have come a long way!"</li>
-      <li>"Obvious examples such as Google Maps &amp; Apple Maps"</li>
-      <li>"Not so obvious like OpenStreetMap"</li>
-      <li>"OSM is free, and open source data"</li>
       <li>"How can I render that data in my game?"</li>
       <li>"First, how do we even render a map?"</li>
       <li>"Needs a <i>slippy map</i> UI (ie; the map that <i>slips</i> around under your finger</li>
@@ -44,6 +34,12 @@ navigator.geolocation.watchPosition(position => {
       <li><code><a-map /></code></li>
       <li>"As few lines of code as the 2D example!"</li>
     </ul>
+  </Notes>
+)
+
+export default (props) => (
+  <Subtitle>
+    <h1>Location Aaware</h1>
     {props.showNotes ? <SlideNotes {...props} /> : ''}
-  </ContentSlide>
+  </Subtitle>
 );
