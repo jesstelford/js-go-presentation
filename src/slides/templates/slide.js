d81028a0bf1a518145ff04c635c671b7e47c6449
Jess Telford <jess+github@jes.st>
2016-11-29 22:23:38 +1100

    Correct slide heading alignment

--- src/slides/templates/slide.js
+++ src/slides/templates/slide.js
@@ -5,7 +5,6 @@ export default styled(ContentSlide)`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
-  padding: 10vh 0;
   text-align: center;
 
   & h1 {
--- src/slides/whoami.js
+++ src/slides/whoami.js
@@ -9,6 +9,10 @@ const Online = styled.div`
   font-size: 8rem;
 `;
 
+const StyledSlide = styled(Slide)`
+  padding: 10vh 0;
+`;
+
 const SlideNotes = (props) => (
   <Notes {...props}>
     <ul>
@@ -24,11 +28,11 @@ const SlideNotes = (props) => (
 )
 
 export default (props) => (
-  <Slide>
+  <StyledSlide>
     <h1>Hi, I'm Jess Telford 👋</h1>
     <Online>@jesstelford</Online>
     <Online><a href="https://bit.ly/js-go-talk">bit.ly/js-go-talk</a></Online>
 
     {props.showNotes ? <SlideNotes {...props} /> : ''}
-  </Slide>
+  </StyledSlide>
 );
